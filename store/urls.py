5d7495eb1147ab56f2a9c5171d0fddee82952352
cisse17 <119404406+cisse17@users.noreply.github.com>
2025-08-20 18:49:54 +0200

    gestion des panier: creation de notre vue pour ajouter les articles dans  notre panier

--- store/urls.py
+++ store/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 from django.conf.urls.static import static
 from shop import settings
 
-from .views import index, product_detail
+from .views import index, product_detail, add_to_cart
 from accounts.views import signup, logout_user, login_user
+
 
 urlpatterns = [
     path('', index, name='boutique'),
     path('product/<str:slug>/', product_detail, name='product_detail'),
+    path('product/<str:slug>/add-to-cart', add_to_cart, name='add-to-cart'),
     path("signup/", signup, name="signup"),
     path("logout/", logout_user, name="logout"),
     path("login_user/", login_user, name="login"),
--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from .models import Cart, Order, Product
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
@@ -17,3 +18,18 @@ def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug)
 
     return render(request, 'store/product_detail.html', {'product':product})
+
+def add_to_cart(request, slug):
+    user = request.user
+    product = get_object_or_404(Product, slug=slug)
+    cart, _ = Cart.objects.get_or_create(user=user) # _  c'est une variable definie masi qui ne sera pas utilisé
+    order, created = Order.objects.get_or_create(user=user, product=product)
+
+    if created:
+        cart.orders.add(order)
+        cart.save()
+
+    else:
+        order.quantity += 1
+        order.save()
+    return redirect(reverse("product_detail", kwargs={"slug": slug}))
